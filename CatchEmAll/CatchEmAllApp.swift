f8b9a138b8a1a1514aa7999f3aa031028b2fe025
daniel79438 <daniel79438@gmail.com>
2025-03-24 17:28:59 +0000

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Daniel Harris on 24/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
